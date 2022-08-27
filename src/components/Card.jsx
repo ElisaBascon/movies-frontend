58616dd0a632be9e77cc15cb6d5a838b8adbff84
elisa <bascongarcia@gmail.com>
2022-08-27 14:19:18 +0200

    Iteration 2 Done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-export default function Card() {
+export default function Card(props) {
+  const {movie} = props;
   return (
     // Use to display each one of the movies
-    <div>Card</div>
+    <div>
+      <img src={movie.image} alt="poster"/>
+      <Link to='/movie/:id'><h2 >{movie.title}</h2></Link>
+    </div>
+
   )
 }
+
+
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Outlet } from 'react-router-dom';
+import Card from '../components/Card';
+import axios from 'axios'
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/v1/movies')
+        setMovies(response.data.data);
+      } catch(error){
+        console.error(error)
+      }
+    }
+    getData();
+  }, [])
+
   return (
     <div>
       <h2>Home</h2>
-      {/* Should display a list of all the movies */}
+      {!movies && <p>Loading</p>}
+      {movies && movies.map(movie =>  {
+        return <Card key={movie._id} movie={movie}/>
+      })}
+      <Outlet />
     </div>
   )
 }
