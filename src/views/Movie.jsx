c5ff45839fd4480b91a03f5bd04e47cca5d32d07
elisa <bascongarcia@gmail.com>
2022-08-29 20:15:26 +0200

    Movie details dont work

--- src/views/Movie.jsx
+++ src/views/Movie.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+
 
 export default function Movie() {
-  const params = useParams();
-  console.log(params)
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [movie, setMovie] = useState(null);
+  console.log(id)
+
+useEffect (() => {
+  const getData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8000/api/v1/movies/${id}`);
+      setMovie(response.data.data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  getData();
+}, [id]);
+
+const handleDelete = async () => {
+  try {
+    await axios.delete(`http://localhost:8000/api/v1/movies/${id}`);
+    navigate('/');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
   return (
     <div>
       <h2>Movie details</h2>
-      {/* Should be the detail of one movie  */}
-      {/* Should have a delete button to delete the movie and then redirect to the Home */}
+      {movie && (
+        <div>
+          <img src={movie.image} alt="poster"/>
+          <h4>{movie.title}</h4>
+          <h6>{movie.year}</h6>
+          <h6>{movie.director}</h6>
+          <h6>{movie.duration}</h6>
+          <button onClick={handleDelete}>Delete</button>
+        </div>)}
+        {!movie && <p>Movie not found</p>}
     </div>
   )
 }
